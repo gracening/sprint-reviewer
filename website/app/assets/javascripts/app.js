69950052e0d7453e0a89eda6800356d25ed997ac
gracening <gracening97@gmail.com>
2017-02-05 01:45:30 -0500

    Improve css

--- website/app/assets/javascripts/app.js
+++ website/app/assets/javascripts/app.js
@@ -7,6 +7,9 @@ app.controller("Ctrl", ["$scope", "$log", function($scope, $log) {
     $scope.bad = [];
     $scope.improve = [];
 
+    // var DBNAME = "development";
+    // var db = openDatabase(DBNAME, '1.0', 'ay lmao', 2 * 1024 * 1024);
+
     function testSetUp() {
         for (var i = 0; i < 10; i++) {
             $scope.good.push({
@@ -33,4 +36,13 @@ app.controller("Ctrl", ["$scope", "$log", function($scope, $log) {
     testSetUp();
     $log.debug("set up test data");
 
+    // db.transaction(function(tx) {
+    //     tx.executeSql('INSERT INTO bad VALUES (24, 'emily', 'TEAM_BLUE: 2017-02-06', 'bad stuff yikes hs', '');');
+    // });
+
+    // function importData() {
+
+    // }
+
 }]);
+
