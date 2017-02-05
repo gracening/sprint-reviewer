35e3ba4bb3c049c6e00f92a92f422e06f4895652
gracening <gracening97@gmail.com>
2017-02-05 05:41:37 -0500

    Implement GET for good table

--- website/app/assets/javascripts/app.js
+++ website/app/assets/javascripts/app.js
@@ -1,6 +1,16 @@
 var app = angular.module("reviewer", []);
 
-app.controller("Ctrl", ["$scope", "$q", "$log", function($scope, $q, $log) {
+app.service('dataService', function($http) {
+    this.getData = function() {
+        return $http({
+            method: 'GET',
+            url: 'http://localhost:3000/goods'
+        });
+        $log.debug("get complete");
+    }
+});
+
+app.controller("Ctrl", ["$scope", "$q", "$log", "dataService", function($scope, $q, $log, dataService) {
     $log.debug("Initializing Controller");
 
     $scope.good = [];
@@ -39,19 +49,20 @@ app.controller("Ctrl", ["$scope", "$q", "$log", function($scope, $q, $log) {
 
     // }
 
-    // dataService.getData().then(function(dataResponse) {
-    //     $scope.good = dataResponse;
-    // });
+    dataService.getData().then(function(res) {
+        $log.debug(res.data.length);
+        for (var i = 0; i < res.data.length; i++) {
+            $scope.good.push({
+                id: res.data[i].id,
+                body: res.data[i].body,
+                author: res.data[i].author,
+                date: res.data[i].date
+            });
+        }
+        $log.debug("Successfully retrieved good data");
+    });
 
-    testSetUp();
+    //testSetUp();
     $log.debug("set up test data");
 }]);
 
-// app.service('dataService', function($http) {
-//     this.getData = function() {
-//         return $http({
-//             method: 'GET',
-//             url: 'http://localhost:3000/goods'
-//         });
-//     }
-// });
