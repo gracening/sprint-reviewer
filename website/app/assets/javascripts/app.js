a85b531c5e35add7e8883e7f1f3437121d14b265
gracening <gracening97@gmail.com>
2017-02-05 05:09:34 -0500

    Change some colours

--- website/app/assets/javascripts/app.js
+++ website/app/assets/javascripts/app.js
@@ -1,11 +1,13 @@
 var app = angular.module("reviewer", []);
 
-app.controller("Ctrl", ["$scope", "$log", function($scope, $log) {
+app.controller("Ctrl", ["$scope", "$q", "$log", function($scope, $q, $log) {
     $log.debug("Initializing Controller");
 
     $scope.good = [];
     $scope.bad = [];
     $scope.improve = [];
+
+    //$scope.data = null;
 
     // var DBNAME = "development";
     // var db = openDatabase(DBNAME, '1.0', 'ay lmao', 2 * 1024 * 1024);
@@ -33,16 +35,23 @@ app.controller("Ctrl", ["$scope", "$log", function($scope, $log) {
         }
     }
 
-    testSetUp();
-    $log.debug("set up test data");
-
-    // db.transaction(function(tx) {
-    //     tx.executeSql('INSERT INTO bad VALUES (24, 'emily', 'TEAM_BLUE: 2017-02-06', 'bad stuff yikes hs', '');');
-    // });
-
     // function importData() {
 
     // }
 
+    // dataService.getData().then(function(dataResponse) {
+    //     $scope.good = dataResponse;
+    // });
+
+    testSetUp();
+    $log.debug("set up test data");
 }]);
 
+// app.service('dataService', function($http) {
+//     this.getData = function() {
+//         return $http({
+//             method: 'GET',
+//             url: 'http://localhost:3000/goods'
+//         });
+//     }
+// });
